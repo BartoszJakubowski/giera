296fde7936e00b8eef3390ee04b7dab75c199085
BartoszJakubowski <bartoszjakubowski96@gmail.com>
2018-08-07 16:35:54 +0200

    koniec

---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+$('.loginLittle').click(function(){
+    $('.loginContent').addClass("block");
+    $('.registerContent').addClass("none");
+    
+});
+
+$('.registerLittle').click(function(){
+    $('.loginContent').addClass("none");
+    $('.registerContent').addClass("block");
+});
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Slovians</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/app.css">
+    <link href="https://fonts.googleapis.com/css?family=Germania+One|Modern+Antiqua" rel="stylesheet">
 </head>
 <body>
     <header>
@@ -13,11 +16,44 @@
 
     <div class="login">
 
-        <p><input type="text"></p>
+        <div class="logOrPas">
+            <div class="loginLittle">
+                <p>LOGIN</p>
+            </div>
 
-        <input type="password">
-        
+            <div class="registerLittle">
+                <p>REJESTRACJA</p>
+            </div>
+        </div>
+
+        <div class="loginContent">
+            <h1>SLOVIANS</h1>
+            <img class="miecze" src="img/miecze.png" >
+
+            <p><input class="textbox textbox1" type="text"></p>
+
+            <p><input class="textbox" type="password"></p>
+
+            <button class="button">Login</button>
+        </div>
+
+        <div class="registerContent">
+            <h1>SLOVIANS</h1>
+            <img class="miecze" src="img/miecze.png" >
+
+            <p><input class="textbox textbox1" type="text"></p>
+
+            <p><input class="textbox" type="password"></p>
+
+            <button class="button">Login</button>
+        </div>
+
     </div>
 
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<script src="js/main.js"></script>
 </body>
 </html>
